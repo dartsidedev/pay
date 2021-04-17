9efef455fd7b29882edd0b4e79aae90364eb69b8
Vince Varga <vince.varga@serialdeveloper.com>
2021-04-17 10:46:13 +0200

    Initialize project

---
+++ example/pay_example.dart
@@ -0,0 +1,6 @@
+import 'package:pay/pay.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/pay_test.dart
@@ -0,0 +1,16 @@
+import 'package:pay/pay.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
